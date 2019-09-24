63ba1b7fec5eab12f9f4f98bbe8689cf67393284
JuliaAvsyuk <julia.avsyuk@mail.ru>
2019-09-25 02:05:19 +0300

    no message

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export class Header extends React.Component {
+    render(){
+        return(
+            <nav className="navbar navbar-default">
+                     <div>
+                         <a href="https://www.youtube.com/">Header</a>
+                     </div>
+            </nav>
+        );
+    }
+}
